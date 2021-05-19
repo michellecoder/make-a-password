9b393a8cbd1501a62549ab9eba0298b05d99dffe
Michelle Oakley <michellecoder38@gmail.com>
2021-05-18 20:48:54 -0700

    Added more code

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ".", "?", "+", "-", "_", "=", ";", ":", "<", ">", "~", "{", "}"];
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Ask the use for how many chracters.
 
-  passwordText.value = password;
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+    prompt("please enter you password length from 8 to 128");
+
+
+
+}
+var passwordLength = password
+
+if (password < 8 || password > 128) {
+
+    window.alert("you need 8 to 128 characters to continue");
+
+
+
+}
